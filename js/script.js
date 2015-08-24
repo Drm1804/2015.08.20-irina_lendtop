1699fd7eeb0e809aeccfce4214545f090a447a35
Drm-a <drm-a@yandex.ru>
2015-08-24 21:15:52 +0500

    Сделал мобильную версию со всеми скриптами

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ $(document).ready(function(){
         var scrollHeight = scroll.height();
         var windowHeight = $(window).height();
         $(window).on('mousewheel', function(event){
-            console.log(event.originalEvent.deltaY);
             var sc = $(document).scrollTop();
             var img = $('.b-scroll-2-big-img');
             if(scrollOffset.top < sc + windowHeight &&  scrollOffset.top + scrollHeight > sc){
@@ -24,7 +23,6 @@ $(document).ready(function(){
                 } else {
                     top -= 30;
                 }
-
                 img.css('top', ''+top+'px');
                 img.attr('data-img', ''+top+'');
 
@@ -33,9 +31,7 @@ $(document).ready(function(){
                 img.attr('data-img', '0');
             }
         });
-        //$(document).scroll(function(event){
-        //
-        //})
+
     };
     paralax();
 
@@ -63,4 +59,66 @@ $(document).ready(function(){
     };
 
     slider();
+
+
+
+    function MobileSpoil(){
+        var _this = this;
+        var items = $('.b-scroll-3-box__item');
+        var btn = $('.b-scroll-3-also-btn');
+        this.whatDevice = function(){
+            if($(window).width() < 690){
+                return true;
+            } else{
+                if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+
+                } else{
+                    return false;
+                }
+                return false;
+            }
+
+        };
+        this.hideAndShow = function(){
+              var count = btn.attr('data-open');
+            items.hide();
+            for(var i = 0; i < count; i++){
+                items.eq(i).show();
+            }
+        };
+        this.addCount = function(){
+            var count = parseInt(btn.attr('data-open'));
+            count +=1.5;
+            btn.attr('data-open',''+count+'');
+            _this.hideAndShow();
+
+        }
+    }
+
+    var mobile = new MobileSpoil();
+    $(window).resize(function(){
+        if(mobile.whatDevice()){
+            mobile.hideAndShow();
+            $('.b-scroll-3-also-btn').on('click', function(){
+                mobile.addCount();
+            })
+        } else {
+            $('.b-scroll-3-box__item').show();
+            $('.b-scroll-3-also-btn').off()
+        }
+    });
+
+    $(window).load(function(){
+        if(mobile.whatDevice()){
+            mobile.hideAndShow();
+            $('.b-scroll-3-also-btn').on('click', function(){
+                mobile.addCount();
+            })
+        } else {
+            $('.b-scroll-3-box__item').show();
+            $('.b-scroll-3-also-btn').off()
+        }
+    });
+
+
 });
