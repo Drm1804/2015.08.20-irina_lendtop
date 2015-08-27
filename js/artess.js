c369d302f9c89fbae2229c286530c948d50e0d3d
Drm-a <drm-a@yandex.ru>
2015-08-27 21:02:42 +0500

    Сделал прокрутку картинок при наведении

--- js/artess.js
+++ js/artess.js
@@ -1,8 +1,38 @@
 $(document).ready(function(){
+
+
     $(window).load(function(){
          $('.b-a-scroll-7').height(
             $('.b-a-sc7__img').height()
         );
 
     });
+
+
+    //$('.b-a-sc6-frame__img').hover(
+    //
+    //)
+    function Artess(){
+        var _this = this;
+        this.sc7 = function(){
+
+        };
+
+        this.scrollImg = function(object){
+            var parent = object.parent();
+            object.hover(
+                function(){
+                    var path = parseInt(object.height()) - parseInt(parent.height())
+                    $(this).stop().animate({top : ''+-path+''}, 3000)
+                },
+                function(){
+                    $(this).stop().animate({top : '0'}, 3000)
+                }
+            )
+        }
+    }
+
+    var artess = new Artess();
+    artess.scrollImg($('.b-a-sc6-frame__img'));
+    artess.scrollImg($('.b-a-sc8-box-img'));
 });
