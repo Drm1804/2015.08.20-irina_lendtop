83f2faa5de20867bbc1326f3ad5ac44e0c797cf1
Drm-a <drm-a@yandex.ru>
2015-08-27 20:18:03 +0500

    Сделал паралакс на странице АРТЕСС

--- js/artess.js
+++ js/artess.js
@@ -1,26 +1,8 @@
 $(document).ready(function(){
-    function Artress(){
-        this.addPositionFixed = function(block){
-            var endBlock = parseInt(block.offset().top)+parseInt(block.height());
-            $(document).on('scroll', function(){
-                var sc = $(document).scrollTop() + $(window).height();
-                var scroll = block.attr('data-scroll');
-                scroll = parseInt(scroll) - 100
-                if(endBlock < sc){
-                    block.css('position', 'relative');
-                    block.css('margin-bottom', ''+scroll+'px');
-                    block.attr('data-scroll', ''+scroll+'')
-                } else {
+    $(window).load(function(){
+         $('.b-a-scroll-7').height(
+            $('.b-a-sc7__img').height()
+        );
 
-                }
-            });
-
-
-        }
-
-    }
-
-    var artress = new Artress();
-
-    artress.addPositionFixed($('.b-a-scroll-6'));
+    });
 });
